13c7b6bef6eea862d819c28fb4ef638575fc95c9
Adrian Esquivel <adrian@flintdigital.com>
2016-01-20 12:20:17 -0600

    Making customer account fields use 2 decimal digits.

--- app/code/local/EmjaInteractive/PurchaseorderManagement/Model/Observer.php
+++ app/code/local/EmjaInteractive/PurchaseorderManagement/Model/Observer.php
@@ -17,18 +17,41 @@ class EmjaInteractive_PurchaseorderManagement_Model_Observer
         }
     }
 
+    //ORIGINAL
+//    public function disablePoTermsEdit(Varien_Event_Observer $observer)
+//    {
+//        if (Mage::getSingleton('admin/session')->isAllowed('purchaseorder/edit_terms')) {
+//            return $this;
+//        }
+//        /** @var Mage_Adminhtml_Block_Customer_Edit_Tab_Account $block */
+//        $block = $observer->getBlock();
+//        if ($block instanceof Mage_Adminhtml_Block_Customer_Edit_Tab_Account) {
+//            $elements = array('net_terms', 'po_limit', 'po_credit');
+//            foreach ($elements as $elementId) {
+//                if ($block->getForm()->getElement($elementId)) {
+//                    $block->getForm()->getElement($elementId)->setDisabled('disabled');
+//                }
+//            }
+//        }
+//    }
+    
+    //HACKED TO SET DECIMAL DIGITS ON ACCOUNT PAGE
     public function disablePoTermsEdit(Varien_Event_Observer $observer)
     {
-        if (Mage::getSingleton('admin/session')->isAllowed('purchaseorder/edit_terms')) {
-            return $this;
-        }
-        /** @var Mage_Adminhtml_Block_Customer_Edit_Tab_Account $block */
+        $disable = !Mage::getSingleton('admin/session')->isAllowed('purchaseorder/edit_terms');
+        
         $block = $observer->getBlock();
         if ($block instanceof Mage_Adminhtml_Block_Customer_Edit_Tab_Account) {
             $elements = array('net_terms', 'po_limit', 'po_credit');
             foreach ($elements as $elementId) {
                 if ($block->getForm()->getElement($elementId)) {
-                    $block->getForm()->getElement($elementId)->setDisabled('disabled');
+                    if(in_array($elementId, array('po_limit', 'po_credit'))) {
+                        $value = number_format($block->getForm()->getElement($elementId)->getValue(), 2, '.', '');
+                        $block->getForm()->getElement($elementId)->setValue($value);
+                    }
+                    
+                    if($disable)
+                        $block->getForm()->getElement($elementId)->setDisabled('disabled');
                 }
             }
         }
